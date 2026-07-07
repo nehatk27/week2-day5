cbbd980eb2bd2d2adf8359d406f9bdb0ed0a0d20
nehatk27 <neha.tk@vonnue.com>
2026-07-07 08:51:56 +0530

    feat: add w3d5t2 - wire all core features

--- js/components/nav.js
+++ js/components/nav.js
@@ -1,10 +1,17 @@
-const navBar = document.querySelector("nav");
+const navBar = document.getElementById("nav");
 navBar.innerHTML = `
-    <ul>
+    <ul id="ul">
         <li><a href="./home.html">Home</a></li>
         <li><a href="./about.html">About</a></li>
         <li><a href="./services.html">Services</a></li>
         <li><a href="./team.html">Team</a></li>
-        <li><a href="./contact.html" aria-current="page">Contact</a></li>
+        <li><a href="./contact.html">Contact</a></li>
+        <li><a href="./gallery.html">Gallery</a></li>
+        <li><a href="./blog.html">Blogs</a></li>
     </ul>
+    <button id="theme-toggle" aria-label="Toggle Dark Mode" aria-pressed="false">
+        Toggle Theme
+    </button>
+
+    <button id="hamburger" aria-expanded="false">&#9776;</button>
 `;
---
+++ js/components/scrollAnimation.js
@@ -0,0 +1,61 @@
+const handleIntersection = (entries, guard) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+
+      guard.unobserve(entry.target);
+    }
+  });
+};
+
+const observerOptions = {
+  root: null, // Use the main screen viewport
+  threshold: 0.5, // Trigger when 50% of the element is visible
+};
+const animatorObserver = new IntersectionObserver(
+  handleIntersection,
+  observerOptions,
+);
+
+const headings = document.querySelectorAll("main h2");
+const articles = document.querySelectorAll(".fade-in-element");
+
+// for home page animation
+const blockquotes = document.querySelectorAll("blockquote");
+blockquotes.forEach((blockquote) => animatorObserver.observe(blockquote));
+
+headings.forEach((heading) => animatorObserver.observe(heading));
+articles.forEach((article) => animatorObserver.observe(article));
+
+// -----Progress bar------
+const progressBar = document.getElementById("progress-bar");
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.scrollY;
+  const totalHeight = document.documentElement.scrollHeight;
+  const screenHeight = window.innerHeight;
+  const scrollableDistance = totalHeight - screenHeight;
+  if (scrollableDistance <= 0) return;
+
+  const scrollPercentage = (currentScroll / scrollableDistance) * 100;
+
+  progressBar.style.width = `${scrollPercentage}%`;
+});
+
+// ----- Back to Top Button  -----
+const backToTopBtn = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.classList.add("show");
+  } else {
+    backToTopBtn.classList.remove("show");
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
